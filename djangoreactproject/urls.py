b69d6c27dfd401aa3e407dba354712ae1bebe953
= <=>
2019-07-17 13:11:57 +0300

    my first django project completed

--- djangoreactproject/urls.py
+++ djangoreactproject/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^api/customers/$', views.customers_list),
+    url(r'^api/customers/$', views.customer_list),
     url(r'^api/customers/(?P<pk>[0-9]+)$', views.customers_detail),
 ]
